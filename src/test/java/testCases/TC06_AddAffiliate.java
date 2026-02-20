43191252f6a006d252c01615b20b7404bfd95449
qa-julienne <juliennemckee7@gmail.com>
2026-02-20 10:27:32 -0500

    Update email used in login test case

--- src/test/java/testCases/TC06_AddAffiliate.java
+++ src/test/java/testCases/TC06_AddAffiliate.java
@@ -45,7 +45,7 @@ public class TC06_AddAffiliate extends BaseClass {
             LoginPage lp = new LoginPage(getDriver());
 
             logger.debug("Entering login credentials");
-            lp.setEmail("juliennemckee7@gmail.com");
+            lp.setEmail("test@email.com");
             lp.setPwd("apple1");
             lp.clickLogin();
 
